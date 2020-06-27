0348d08312368647a1fdd3ada991c0a1269245cc
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-06-27 11:16:46 -0600

    working api call for linked in svg

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// Initializing the cors middleware
+const cors = Cors( {
+  methods: [ "GET", "HEAD" ],
+} );
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware( req, res, fn ){
+  debugger;
+  return new Promise( ( resolve, reject ) => {
+    debugger;
+    fn( req, res, ( result ) => {
+      if( result instanceof Error ){
+        return reject( result );
+      }
+      
+      return resolve( result );
+    } );
+  } );
 }
+
+async function handler( req, res ){
+  debugger;
+  // Run the middleware
+  await runMiddleware( req, res, cors );
+  
+  // Rest of the API logic
+  res.json( { message: "Hello Everyone!" } );
+}
+
+export default handler;
