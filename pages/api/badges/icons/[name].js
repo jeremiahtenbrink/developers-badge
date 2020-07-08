b6d046dbad0f7b1e985128bca9ae4b45fb705337
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2020-07-07 21:25:58 -0600

    changed function to account for promise being returned.

--- pages/api/badges/icons/[name].js
+++ pages/api/badges/icons/[name].js
@@ -7,13 +7,18 @@ async function handler( req, res ){
   // Run the middleware
   runCoors( req, res )
     .then( result => {
-      debugger;
-      const svg = getIconSvg( name );
-      res.setHeader( "content-type", "image/svg+xml" );
-      res.send( svg );
+      getIconSvg( name ).then( svg => {
+        res.setHeader( "content-type", "image/svg+xml" );
+        res.send( svg );
+      } ).catch( err => {
+        console.log( err.message );
+        res.status( err.status || 500 ).json( { error: err.message } );
+      } );
+      
     } ).catch( err => {
     
     // Rest of the API logic
+    console.log( err.message );
     res.status( 400 ).json( { message: err.message } );
   } );
   
